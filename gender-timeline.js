9dcb45f13d2a3acf394ddae5e0dc2c62839f3f39
JLS <60688597+jon-lapresa-serrano@users.noreply.github.com>
2021-04-23 10:29:25 +0200

    Update gender-timeline.js

--- gender-timeline.js
+++ gender-timeline.js
@@ -9,7 +9,7 @@ function new_timeline() {
       type: "survey-multi-choice",
       questions: [
         {prompt: "Por favor indique su género.", name: 'Gender', options: gend, required: true},
-        {prompt: "¿Su género coincide con el que le asignaron al nacer?", name: 'CisTrans', options: y_n, required: true}
+        {prompt: "¿Su género coincide con el que le asignaron al nacer?", name: 'Cis', options: y_n, required: true}
       ],
       post_trial_gap: 500
   }],
