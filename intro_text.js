5348aecdbbe89bd95546e28d573f019108b11757
JLS <60688597+jon-lapresa-serrano@users.noreply.github.com>
2021-05-10 14:54:33 +0200

    Update intro_text.js

--- intro_text.js
+++ intro_text.js
@@ -1,8 +1,8 @@
 var  intro_text =  [
   {
-    "stimulus": "<p>Muchas gracias por aceptar participar en este estudio psicolingüístico.<\/p> <p>Por favor pulse 'Continuar' para comenzar con una encuesta previa al experimento.<\/p>",
+    "stimulus": "<p>Muchas gracias por aceptar participar en este estudio psicolingüístico.<\/p> <p>Por favor pulse 'Continuar' para comenzar con una encuesta previa al experimento.<\/p> <p> Al pulsarlo usted también da su consentimiento para que podamos guardar los datos que nos proporcione a lo largo del experimento.</p> <p>Le recordamos que ninguno de estos datos es personal y que todos ellos serán guardados de forma completamente anónima.</p> <p>También le recordamos que puede interrumpir su participación en cualquier momento durante el experimento.</p>",
     "data": [
-      "<p>Muchas gracias por aceptar participar en este estudio psicolingüístico.<\/p> <p>Por favor pulse 'Continuar' para comenzar con una encuesta previa al experimento.<\/p>"
+      "<p>Muchas gracias por aceptar participar en este estudio psicolingüístico.<\/p> <p>Por favor pulse 'Continuar' para comenzar con una encuesta previa al experimento.<\/p> <p> Al pulsarlo usted también da su consentimiento para que podamos guardar los datos que nos proporcione a lo largo del experimento.</p> <p>Le recordamos que ninguno de estos datos es personal y que todos ellos serán guardados de forma completamente anónima.</p> <p>También le recordamos que puede interrumpir su participación en cualquier momento durante el experimento.</p>"
     ]
   }
 ]
